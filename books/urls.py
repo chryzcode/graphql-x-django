898f06ff1c0f48937ab8aebc4018d99523a32c9a
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-02-15 02:49:55 +0100

    push

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt 
 from graphene_django.views import GraphQLView
 from books.schema import schema
 
 urlpatterns = [
-    path('graphql', GraphQLView.as_view(graphiql=True, schema=schema)),
+    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
