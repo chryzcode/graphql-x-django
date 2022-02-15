2e3ea6e326f5cfdbdd71eb3d85f843d676891358
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-02-15 04:02:26 +0100

    push

--- core/settings.py
+++ core/settings.py
@@ -36,8 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'books',
     'graphene_django',
+
+    'books',
+    'quiz',
 ]
 
 
---
+++ quiz/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.views.decorators.csrf import csrf_exempt 
+from graphene_django.views import GraphQLView
+from quiz.schema import schema
+
+urlpatterns = [
+    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
+]
