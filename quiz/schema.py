d5ce5528cbc98a060c07deebc1f8e9c487c5f172
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-02-15 16:15:30 +0100

    commit

--- quiz/schema.py
+++ quiz/schema.py
@@ -25,6 +25,20 @@ class AnswerType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     
-    all_quizzes = DjangoListField(QuizzesType)
+    # all_quizzes = DjangoListField(QuizzesType)
+    all_quizzes = graphene.Field(QuizzesType, id=graphene.Int())
+    all_answers = graphene.List(AnswerType, id=graphene.Int())
+    all_questions = graphene.Field(QuestionType, id=graphene.Int())
+
+    def resolve_all_quizzes(root, info, id):
+        return Quizzes.objects.get(pk=id)
+
+    def resolve_all_questions(root, info, id):
+        return Question.objects.get(pk=id)
+
+    def resolve_all_answers(root, info, id):
+        return Answer.objects.filter(question=id)
+    
+
 
 schema = graphene.Schema(query=Query)
