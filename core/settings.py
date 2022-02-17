d11e05e2d7ecd636af709498f36cca25e8af8d08
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-02-17 01:36:01 +0100

    push

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'books',
     'quiz',
+    'users',
 ]
 
 
@@ -122,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'users.ExtendedUser'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class ExtendedUser(AbstractUser):
+    
+    email = models.EmailField(unique=True, blank=False, max_length=255, verbose_name='email') 
+
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
