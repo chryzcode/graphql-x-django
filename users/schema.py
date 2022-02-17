f90d761567bd4309bc496f39ecd466d6fbed8230
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-02-18 00:44:30 +0100

    push

--- users/schema.py
+++ users/schema.py
@@ -7,6 +7,10 @@ class AuthMutation(graphene.ObjectType):
     register = mutations.Register.Field()
     verify_account = mutations.VerifyAccount.Field()
     token_auth = mutations.ObtainJSONWebToken.Field()
+    update_account = mutations.UpdateAccount.Field()
+    resend_activation_email = mutations.ResendActivationEmail.Field()
+    send_password_reset_email = mutations.SendPasswordResetEmail.Field()
+    password_reset = mutations.PasswordReset.Field()
 
 class Query(UserQuery, MeQuery, graphene.ObjectType):
     pass 
