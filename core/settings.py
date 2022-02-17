030536b5dba6bb70527248657f45861e0aba620c
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-02-17 01:46:16 +0100

    commit

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
+    'graphql_jwt.refresh_token.apps.RefreshTokenConfig',
 
     'books',
     'quiz',
